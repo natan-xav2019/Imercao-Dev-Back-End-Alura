63be75200fe06ef556cf582233f2ab28bca78433
Natan Xavier <natanxaviersiquera@gmail.com>
2024-11-25 10:49:21 -0300

    starting multer

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
+import multer from "multer";
 import { listarPosts, postarNovoPosts } from "../controllers/postsControllers.js";
+
+const upload = multer({ dest: './uploads' })
 
 const routes = (app) => {
     app.use(express.json());
 
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPosts);
+    app.post("/upload", upload.single("imagem"),)
 }
 
 export default routes;
