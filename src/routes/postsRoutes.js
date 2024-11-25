733954dbe1b1002132dd81b16f73316ff18f2529
Natan Xavier <natanxaviersiquera@gmail.com>
2024-11-25 13:00:16 -0300

    config multer windows

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,15 +1,24 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPosts } from "../controllers/postsControllers.js";
+import { listarPosts, postarNovoPosts, uploadImagem } from "../controllers/postsControllers.js";
 
-const upload = multer({ dest: './uploads' })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage });
 
 const routes = (app) => {
     app.use(express.json());
 
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPosts);
-    app.post("/upload", upload.single("imagem"),)
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
