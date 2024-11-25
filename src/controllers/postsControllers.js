7fa9ba63af7f4db3ba6a02a52d6d5cf8c99afe1b
Natan Xavier <natanxaviersiquera@gmail.com>
2024-11-25 15:19:37 -0300

    add description generator

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,5 +1,7 @@
 import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModels.js";
 import fs from "fs"
+import gerarDescricaoComGemini from "../services/geminiService.js"
+
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
@@ -37,13 +39,16 @@ export async function uploadImagem(req, res) {
 export async function AtualzarNovoPost(req, res) {
     const id = req.params.id;
     const urlImagem = `http://localhost:3000/${id}.png`
-
-    const post = {
-        imgUrl: urlImagem,
-        descricao: req.body.descricao,
-        alt: req.body.alt
-    }
     try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer)
+
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+
         const postCriado = await atualizarPost(id, post);
         res.status(200).json(postCriado);
     } catch (erro) {
