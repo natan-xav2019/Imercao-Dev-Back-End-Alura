ae865373ebcab39e5ba754a45d79b7b723de7508
Natan Xavier <natanxaviersiquera@gmail.com>
2024-11-25 13:00:38 -0300

    add new func uploadImagem

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,5 +1,5 @@
 import { getTodosPosts, criarPost } from "../models/postsModels.js";
-
+import fs from "fs"
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
@@ -13,5 +13,23 @@ export async function postarNovoPosts(req, res) {
     } catch (erro) {
         console.error(erro.menssage);
         res.status(500).json({ Erro: "Falha na requisição" })
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);
+    } catch (erro) {
+        console.error(erro.message);
+        res.status(500).json({ "Erro": "Falha na requisição" })
     }
 }
