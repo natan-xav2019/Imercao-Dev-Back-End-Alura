1fa0baabd5eb4e0e2d66a8ae7e6ea821a3a3e2b6
Natan Xavier <natanxaviersiquera@gmail.com>
2024-11-25 10:32:08 -0300

    add new func postarNovoPosts

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -10,3 +10,8 @@ export async function getTodosPosts() {
     const colecao = db.collection("posts");
     return colecao.find().toArray();
 }
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import { listarPosts, postarNovoPosts } from "../controllers/postsControllers.js";
 
 const routes = (app) => {
     app.use(express.json());
 
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPosts);
 }
 
 export default routes;
